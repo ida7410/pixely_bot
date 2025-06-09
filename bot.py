ef0631e6287f35892197b8a45cda6489b0f90641
ida741 <giruida@gmail.com>
2025-06-09 17:16:45 -0400

    add from webdriver_manager.chrome import ChromeDriverManager in bot.py

--- bot.py
+++ bot.py
@@ -11,6 +11,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service  as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 from config import *
 
 import os
