774fe3e12be6b74a0f94d2fb50710e9974ba9e8b
ida741 <giruida@gmail.com>
2025-06-09 17:09:43 -0400

    add from selenium import webdriver in bot.py

--- bot.py
+++ bot.py
@@ -9,6 +9,7 @@ import feedparser
 
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
 from config import *
 
 import os
